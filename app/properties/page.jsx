7d5f7c1695df7395bf70830991ac03b7ae6acb9a
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-24 13:42:47 +0800

    feat: renaming properties.json into properties.js so it can be imported

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,8 @@
-import Link from "next/link"
-
+import properties from '@/properties'
 const PropertiesPage = () => {
   return (
     <div>
-        <h1 className="text-2xl">Welcome</h1>
-        <Link href='/'>Home Page</Link>
+        
     </div>
   )
 }
