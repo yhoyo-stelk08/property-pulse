3df55b3eed278f1d0c144ca709aff738f66f378e
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-24 10:44:52 +0800

    feat: learn about useRouter, useParams, useSearchParams, usePathname.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+'use client';
+import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 const PropertyPage = () => {
+  const router = useRouter();
+  const {id} = useParams();
+  const searchParams = useSearchParams();
+  const name = searchParams.get('name')
+  const pathName = usePathname();
   return (
-    <div>Property Page</div>
-  )
-}
+    <div>
+      <button className="p-3 bg-blue-500" onClick={() => router.push('/')}>
+        Go Home {id} {name} {pathName}
+      </button>
+    </div>
+  );
+};
 
-export default PropertyPage
+export default PropertyPage;
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Link from "next/link"
 
 const PropertiesPage = () => {
   return (
-    <div>Properties Page</div>
+    <div>
+        <h1 className="text-2xl">Welcome</h1>
+        <Link href='/'>Home Page</Link>
+    </div>
   )
 }
 
