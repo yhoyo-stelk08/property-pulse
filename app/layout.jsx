51351a3256f9f0ad96bd2c837cb8d59bfe87b327
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-24 13:35:34 +0800

    feat: make new components Footer.jsx make new components Footer and use it in layout.jsx
     Changes to be committed:
    	modified:   app/layout.jsx
    	new file:   components/Footer.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@/assets/styles/global.css'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export const metadata = {
   title: 'PropertyPulse | Find your perfect rental',
@@ -14,6 +15,7 @@ const MainLayout = ({children}) => {
       <body>
         <Navbar></Navbar>
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
