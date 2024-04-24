e7acc30854d0879674c07b9f07360af6a102dbdd
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-24 10:22:42 +0800

    feat: make new components make new components for list all properties and single property and make necessary changes in the homepage.
     Changes to be committed:
    	modified:   app/page.jsx
    	new file:   app/properties/[id]/page.jsx
    	new file:   app/properties/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Link from "next/link"
 
 const HomePage = () => {
   return (
-    <div>Home Page</div>
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href='./properties'>Show Properties</Link>
+    </div>
   )
 }
 
---
+++ app/properties/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PropertiesPage = () => {
+  return (
+    <div>Properties Page</div>
+  )
+}
+
+export default PropertiesPage
