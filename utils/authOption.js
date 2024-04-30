86708bec3d8653e776f0c8b994f789e08298c792
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-30 23:39:41 +0800

    feat: configure next-auth

---
+++ utils/authOption.js
@@ -0,0 +1,32 @@
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOption = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+  callback: {
+    // invoked on successfull signin
+    async signIn({profile}) {
+        // 1. Connect to database
+        // 2. Check if user exists
+        // 3. If not, then add user to database
+        // 4. Return true to allow sign in
+    },
+    // Modifies the session object
+    async session({ session }) {
+        // 1. Get user from database
+        // 2. Assign the user id to the session
+        // 3. Return the session
+    }
+  }
+};
