68b5f3672ef22289e10405e150104ed81fd2deac
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-30 23:43:17 +0800

    feat: wrap the layout with AuthProvider components.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import '@/assets/styles/global.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import React from 'react';
+import '@/assets/styles/global.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
 
 export const metadata = {
   title: 'PropertyPulse | Find your perfect rental',
   description: 'Find your dream rental property',
   keywords: 'rentals, find rentals, find properties',
-}
+};
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
-    <html lang='EN'>
-      <body>
-        <Navbar></Navbar>
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  )
-}
+    <AuthProvider>
+      <html lang="EN">
+        <body>
+          <Navbar></Navbar>
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
