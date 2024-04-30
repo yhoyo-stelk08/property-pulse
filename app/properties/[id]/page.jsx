d39db235f1f9ebe9984b9ff26a9d78c564bf9467
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-30 21:23:57 +0800

    feat: modified page.jsx for single property detail modified the files to fetch data from database.
     Changes to be committed:
    	modified:   app/properties/[id]/page.jsx

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,19 +1,31 @@
 'use client';
-import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import { fetchProperty } from '@/utils/request';
+import { useParams } from 'next/navigation';
 
 const PropertyPage = () => {
-  const router = useRouter();
-  const {id} = useParams();
-  const searchParams = useSearchParams();
-  const name = searchParams.get('name')
-  const pathName = usePathname();
-  return (
-    <div>
-      <button className="p-3 bg-blue-500" onClick={() => router.push('/')}>
-        Go Home {id} {name} {pathName}
-      </button>
-    </div>
-  );
+  const { id } = useParams();
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.log('Error fetching property: ', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+  return <div>Single Property</div>;
 };
 
 export default PropertyPage;
