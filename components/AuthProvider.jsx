86310a0bb83ca2ac8f74cf2fe7c18813d533ab38
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-30 23:42:04 +0800

    feat: make new components AuthProvider.jsx.

---
+++ components/AuthProvider.jsx
@@ -0,0 +1,7 @@
+import { SesionProvider } from 'next-auth/react';
+
+const AuthProvider = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthProvider;
