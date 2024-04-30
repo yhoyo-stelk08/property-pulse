185cbaaefca7c8fc3571b45ac329aff21dbf841a
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-30 23:47:29 +0800

    feat: make the AuthProvider.jsx as client components.

--- components/AuthProvider.jsx
+++ components/AuthProvider.jsx
@@ -1,4 +1,5 @@
-import { SesionProvider } from 'next-auth/react';
+'use client'
+import { SessionProvider } from 'next-auth/react';
 
 const AuthProvider = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
