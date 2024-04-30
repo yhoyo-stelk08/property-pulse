da7e9a1c9af3f360c497afb1ee5340fe6510c583
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-30 20:01:07 +0800

    feat: add route to get all properties from db

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,9 +1,14 @@
 import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
+// GET /api/properties
 export const GET = async (request) => {
   try {
     await connectDB();
-    return new Response(JSON.stringify({ message: 'Next js API response' }), {
+
+    const properties = await Property.find({})
+    console.log(properties)
+    return new Response(JSON.stringify(properties), {
       status: 200,
     });
   } catch (error) {
