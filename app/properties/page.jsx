5b02c2015970a5da2c0a8101ab3b38f672b8ac53
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-30 20:12:49 +0800

    feat: modified /properties/page.jsx. fetch properties data from database instead from properties.json files
     Changes to be committed:
    	modified:   app/properties/page.jsx

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,23 @@
-import { properties } from '@/properties';
 import { PropertyCard } from '@/components/PropertyCard';
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
@@ -10,10 +26,7 @@ const PropertiesPage = () => {
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {properties.map((property) => (
-              <PropertyCard
-                key={property._id}
-                property={property}
-              />
+              <PropertyCard key={property._id} property={property} />
             ))}
           </div>
         )}
