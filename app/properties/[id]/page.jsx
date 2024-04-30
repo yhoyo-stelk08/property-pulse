9803d949d26d6dd56ce728d49708499e1481d328
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-04-30 23:03:59 +0800

    feat: use spinner components in PropertyPage.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -6,6 +6,7 @@ import { FaArrowLeft } from 'react-icons/fa';
 import Link from 'next/link';
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import PropertyDetails from '@/components/PropertyDetails';
+import Spinner from '@/components/Spinner';
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -39,6 +40,7 @@ const PropertyPage = () => {
   }
   return (
     <>
+      {loading && <Spinner loading={loading}/>}
       {!loading && property && (
         <>
           <PropertyHeaderImage image={property.images[0]} />
--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+'use client';
+import ClipLoader from 'react-spinners/ClipLoader';
 
-const Spinner = () => {
+const override = {
+  display: 'block',
+  margin: '100px auto',
+};
+
+import React from 'react';
+
+const Spinner = ({loading}) => {
   return (
-    <div>Spinner</div>
-  )
-}
+    <>
+      <ClipLoader
+        color="#3B82F6"
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+      />
+    </>
+  );
+};
 
-export default Spinner
+export default Spinner;
