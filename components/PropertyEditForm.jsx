421cab0ba439c8eda0cbe305414531a0e4e7f2d9
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-02 22:41:29 +0800

    feat: finish the logic in EditForm.jsx finish the logic in EditForm components and make necessary changes
     Changes to be committed:
    	modified:   app/properties/[id]/edit/page.jsx
    	modified:   components/PropertyEditForm.jsx

---
+++ components/PropertyEditForm.jsx
@@ -0,0 +1,401 @@
+'use client';
+import { useState, useEffect } from 'react';
+import Amenities from './Amenities';
+import { amenitiesArray } from '@/utils/const';
+import { useParams, useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import { fetchProperty } from '@/utils/request';
+
+const PropertyEditForm = () => {
+  const { id } = useParams();
+  const router = useRouter();
+  const [field, setField] = useState({
+    name: '',
+    type: '',
+    description: '',
+    location: {
+      street: '',
+      city: '',
+      state: '',
+      zipcode: '',
+    },
+    beds: '',
+    baths: '',
+    square_feet: '',
+    amenities: [],
+    rates: {
+      nightly: '',
+      weekly: '',
+      monthly: '',
+    },
+    seller_info: {
+      name: '',
+      email: '',
+      phone: '',
+    },
+  });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      try {
+        const propertyData = await fetchProperty(id);
+
+        // check rates for 0, if so replace it with empty string
+        if (propertyData && propertyData.rates) {
+          const defaultRates = { ...propertyData.rates }; // spread the propertyData rates and assign it to defaultRates
+          for (const rate in defaultRates) {
+            if (defaultRates[rate] === null || defaultRates[rate] === 0) {
+              defaultRates[rate] = '';
+            }
+          }
+          propertyData.rates = defaultRates;
+        }
+
+        setField(propertyData);
+      } catch (error) {
+        toast.error('Failed to fetch property data')
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPropertyData();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name.includes('.')) {
+      const [outerKey, innerKey] = name.split('.');
+
+      setField((prevValue) => ({
+        ...prevValue,
+        [outerKey]: {
+          ...prevValue[outerKey],
+          [innerKey]: value,
+        },
+      }));
+    } else {
+      setField((prevValue) => ({
+        ...prevValue,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleAmenitiesChange = (e) => {
+    const { value, checked } = e.target;
+    // clone the amenities array
+    const updatedAmenities = [...field.amenities];
+
+    if (checked) {
+      // add value to array updatedAmenities
+      updatedAmenities.push(value);
+    } else {
+      // Remove value from array
+      const index = updatedAmenities.indexOf(value); // get the index of value in the array
+
+      if (index !== -1) {
+        updatedAmenities.splice(index, 1);
+      }
+    }
+    // update state with updatedAmenities array
+    setField((prevValue) => ({
+      ...prevValue,
+      amenities: updatedAmenities,
+    }));
+  };
+
+  const handleSubmit = () => {};
+
+  return (
+    !loading && (
+      <form onSubmit={handleSubmit}>
+        <h2 className="text-3xl text-center font-semibold mb-6">
+          Edit Property
+        </h2>
+
+        <div className="mb-4">
+          <label htmlFor="type" className="block text-gray-700 font-bold mb-2">
+            Property Type
+          </label>
+          <select
+            id="type"
+            name="type"
+            className="border rounded w-full py-2 px-3"
+            required
+            value={field.type}
+            onChange={handleChange}
+          >
+            <option value="Apartment">Apartment</option>
+            <option value="Condo">Condo</option>
+            <option value="House">House</option>
+            <option value="Cabin Or Cottage">Cabin or Cottage</option>
+            <option value="Room">Room</option>
+            <option value="Studio">Studio</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">
+            Listing Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="border rounded w-full py-2 px-3 mb-2"
+            placeholder="eg. Beautiful Apartment In Miami"
+            required
+            value={field.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="description"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Description
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            className="border rounded w-full py-2 px-3"
+            rows="4"
+            placeholder="Add an optional description of your property"
+            value={field.description}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+
+        <div className="mb-4 bg-blue-50 p-4">
+          <label className="block text-gray-700 font-bold mb-2">Location</label>
+          <input
+            type="text"
+            id="street"
+            name="location.street"
+            className="border rounded w-full py-2 px-3 mb-2"
+            placeholder="Street"
+            value={field.location.street}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            id="city"
+            name="location.city"
+            className="border rounded w-full py-2 px-3 mb-2"
+            placeholder="City"
+            required
+            value={field.location.city}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            id="state"
+            name="location.state"
+            className="border rounded w-full py-2 px-3 mb-2"
+            placeholder="State"
+            required
+            value={field.location.state}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            id="zipcode"
+            name="location.zipcode"
+            className="border rounded w-full py-2 px-3 mb-2"
+            placeholder="Zipcode"
+            value={field.location.zipcode}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="mb-4 flex flex-wrap">
+          <div className="w-full sm:w-1/3 pr-2">
+            <label
+              htmlFor="beds"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Beds
+            </label>
+            <input
+              type="number"
+              id="beds"
+              name="beds"
+              className="border rounded w-full py-2 px-3"
+              required
+              value={field.beds}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="w-full sm:w-1/3 px-2">
+            <label
+              htmlFor="baths"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Baths
+            </label>
+            <input
+              type="number"
+              id="baths"
+              name="baths"
+              className="border rounded w-full py-2 px-3"
+              required
+              value={field.baths}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="w-full sm:w-1/3 pl-2">
+            <label
+              htmlFor="square_feet"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Square Feet
+            </label>
+            <input
+              type="number"
+              id="square_feet"
+              name="square_feet"
+              className="border rounded w-full py-2 px-3"
+              required
+              value={field.square_feet}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">
+            Amenities
+          </label>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+            {amenitiesArray.map((amenity, index) => (
+              <Amenities
+                key={index}
+                id={amenity.id}
+                name="amenities"
+                value={amenity.value}
+                isChecked={field.amenities.includes(amenity.value)}
+                onChange={handleAmenitiesChange}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="mb-4 bg-blue-50 p-4">
+          <label className="block text-gray-700 font-bold mb-2">
+            Rates (Leave blank if not applicable)
+          </label>
+          <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+            <div className="flex items-center">
+              <label htmlFor="weekly_rate" className="mr-2">
+                Weekly
+              </label>
+              <input
+                type="number"
+                id="weekly_rate"
+                name="rates.weekly"
+                className="border rounded w-full py-2 px-3"
+                value={field.rates.weekly}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex items-center">
+              <label htmlFor="monthly_rate" className="mr-2">
+                Monthly
+              </label>
+              <input
+                type="number"
+                id="monthly_rate"
+                name="rates.monthly"
+                className="border rounded w-full py-2 px-3"
+                value={field.rates.monthly}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex items-center">
+              <label htmlFor="nightly_rate" className="mr-2">
+                Nightly
+              </label>
+              <input
+                type="number"
+                id="nightly_rate"
+                name="rates.nightly"
+                className="border rounded w-full py-2 px-3"
+                value={field.rates.nightly}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <label
+            htmlFor="seller_name"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Seller Name
+          </label>
+          <input
+            type="text"
+            id="seller_name"
+            name="seller_info.name"
+            className="border rounded w-full py-2 px-3"
+            placeholder="Name"
+            value={field.seller_info.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="seller_email"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Seller Email
+          </label>
+          <input
+            type="email"
+            id="seller_email"
+            name="seller_info.email"
+            className="border rounded w-full py-2 px-3"
+            placeholder="Email address"
+            required
+            value={field.seller_info.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="seller_phone"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Seller Phone
+          </label>
+          <input
+            type="tel"
+            id="seller_phone"
+            name="seller_info.phone"
+            className="border rounded w-full py-2 px-3"
+            placeholder="Phone"
+            value={field.seller_info.phone}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Update Property
+          </button>
+        </div>
+      </form>
+    )
+  );
+};
+
+export default PropertyEditForm;
