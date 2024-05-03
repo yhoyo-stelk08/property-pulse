0f00b5402de674be473f4ed2ec9fc57b194792ac
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-03 13:58:27 +0800

    feat: add GET request in api bookmarks
     Changes to be committed:
    	modified:   app/api/bookmarks/route.js

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -1,8 +1,35 @@
 import connectDB from '@/config/database';
+import Property from '@/models/Property';
 import User from '@/models/User';
 import { getUserSession } from '@/utils/getUserSession';
 
 export const dynamic = 'force-dynamic';
+
+// GET /api/bookmarks
+export const GET = async (request) => {
+  try {
+    // connect to db
+    await connectDB();
+    // get user session from server
+    const userSession = await getUserSession();
+    // check if the session is set
+    if (!userSession || !userSession.userId) {
+      return new Response('You need to login to use bookmark', { status: 401 });
+    }
+    const { userId } = userSession;
+
+    // find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // get user bookmark
+    const bookmark = await Property.find({_id: {$in: user.bookmark}})
+
+    return new Response(JSON.stringify(bookmark), {status: 200})
+  } catch (error) {
+    console.log(error)
+    return new Response('Error in DB connection', {status: 500})
+  }
+};
 
 // POST /api/bookmarks
 export const POST = async (request) => {
