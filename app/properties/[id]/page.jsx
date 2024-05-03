14004d240254b6fbf746e1f14fa4fc022f579891
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-03 08:23:48 +0800

    feat: make new components BookmarkButton make new components BookmarkButton.jsx and use it in Property Page.
     Changes to be committed:
    	modified:   app/properties/[id]/page.jsx
    	new file:   components/BookmarkButton.jsx

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -8,6 +8,7 @@ import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import PropertyDetails from '@/components/PropertyDetails';
 import Spinner from '@/components/Spinner';
 import PropertyImages from '@/components/PropertyImages';
+import BookmarkButton from '@/components/BookmarkButton';
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -61,9 +62,7 @@ const PropertyPage = () => {
                 <PropertyDetails property={property} />
                 {/* <!-- Sidebar --> */}
                 <aside className="space-y-4">
-                  <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-                    <i className="fas fa-bookmark mr-2"></i> Bookmark Property
-                  </button>
+                  <BookmarkButton property={property} />
                   <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
                     <i className="fas fa-share mr-2"></i> Share Property
                   </button>
