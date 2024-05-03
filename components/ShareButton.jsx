5ee1f50ee52a5eaeb3073ad1ac8ce9219cff4734
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-03 17:19:26 +0800

    feat: use react-share in ShareButton components.

--- components/ShareButton.jsx
+++ components/ShareButton.jsx
@@ -1,10 +1,52 @@
-import { FaShare } from 'react-icons/fa'
+import { FaShare } from 'react-icons/fa';
+import {
+  WhatsappShareButton,
+  WhatsappIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  EmailShareButton,
+  EmailIcon,
+} from 'react-share';
 
-const ShareButton = ({property}) => {
+const ShareButton = ({ property }) => {
+  const urlShare = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}}`;
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <FaShare className="mr-2" /> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2">
+        Share This Property
+      </h3>
+      <div className="flex gap-3 justify-center pb-5">
+        <FacebookShareButton
+          url={urlShare}
+          hashtag={`#${property.type.replace(/\s/g, '')}ForRent`.trim()}
+        >
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={urlShare}
+          title={property.name}
+          hashtags={[`#${property.type.replace(/\s/g, '')}ForRent`]}
+        >
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+        <WhatsappShareButton
+          url={urlShare}
+          title={property.name}
+          separator=':: '
+        >
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+        <EmailShareButton
+          url={urlShare}
+          subject={property.name}
+          body={`Check out this property: ${urlShare}`}
+        >
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+      </div>
+    </>
   );
 };
 
