232402d6483f5a2165dae9ef417e458cb912ae85
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-03 08:56:19 +0800

    feat: add route for bookmark property.

---
+++ app/api/bookmarks/route.js
@@ -0,0 +1,52 @@
+import connectDB from '@/config/database';
+import User from '@/models/User';
+import { getUserSession } from '@/utils/getUserSession';
+
+export const dynamic = 'force-dynamic';
+
+// POST /api/bookmarks
+export const POST = async (request) => {
+  try {
+    // connect to db
+    await connectDB();
+    // get property id from the request body
+    const { propertyId } = request.json();
+    // get user session from server
+    const userSession = getUserSession();
+    // check if the session is set
+    if (!session || !session.userId) {
+      return new Response('You need to login to use bookmark', { status: 401 });
+    }
+
+    const { userId } = userSession;
+
+    // find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // check if the property is already bookmarked
+    let isBookmark = user.bookmark.includes(propertyId);
+
+    let message;
+
+    if (isBookmark) {
+      // if property ]already bookmarked remove it
+      user.bookmark.pull(propertyId);
+      message = 'Bookmark removed successfully';
+      isBookmark = false;
+    } else {
+      // if property not bookmarked add it
+      user.bookmark.push(propertyId);
+      message = 'Property boookmarked sucessfully';
+      isBookmark = true;
+    }
+
+    // save the updated bookmark to db
+    await user.save();
+    return new Response(JSON.stringify({ message, isBookmark }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Error in trying bookmarked property', { status: 500 });
+  }
+};
