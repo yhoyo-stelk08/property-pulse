581a268f207a62c199b4fe5ae482b86ab8cb4599
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-04 17:59:00 +0800

    feat: make new models for Messages.

---
+++ models/Messages.js
@@ -0,0 +1,43 @@
+import { Schema, model, models } from 'mongoose';
+
+const MessageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    recipent: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    property: {
+      type: Schema.Types.ObjectId,
+      ref: 'Property',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: (true, 'Name is required'),
+    },
+    email: {
+      type: String,
+      required: (true, 'Email is required'),
+    },
+    phone: {
+      type: String,
+    },
+    body: {
+      type: String,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamp: true }
+);
+
+const Messages = model.Messages || model('Messages', MessageSchema);
+export default Messages;
