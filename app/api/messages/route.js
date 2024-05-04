7b6da82e6e63ce43e9dbcaa102b61ad207b950e6
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-04 20:59:37 +0800

    feat: make route for POST request /api/messages

---
+++ app/api/messages/route.js
@@ -0,0 +1,47 @@
+import connectDB from '@/config/database';
+import Messages from '@/models/Messages';
+import { getUserSession } from '@/utils/getUserSession';
+
+export const dynamic = 'force-dynamic';
+// POST /api/messsages
+export const POST = async (request) => {
+  try {
+    // connect to db
+    await connectDB();
+    // deconstruct data from
+    const { name, email, phone, message, recipent, property } = request.json();
+    // get user session
+    const userSession = getUserSession();
+
+    if (!userSession || !userSession.userId) {
+      return new Response('User ID is required', { status: 401 });
+    }
+
+    const { user } = userSession;
+
+    // can not send message to self
+    if (user.id === recipent) {
+      return new Response(
+        JSON.stringify({ message: 'Cannot send message to your self' }),
+        { status: 400 }
+      );
+    }
+
+    const newMessage = new Messages({
+      sender: user.id,
+      recipent: recipent,
+      property: property,
+      name: name,
+      email: email,
+      phone: phone,
+      body: message,
+    });
+
+    await newMessage.save();
+
+    return new Response(JSON.stringify({message: 'Message has been sent'}), {status: 200});
+  } catch (error) {
+    console.log(error);
+    return new Response('Error in sending message', {status: 500});
+  }
+};
