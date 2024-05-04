8114b5b609dbd5ec3861e6aaa634ca694476624f
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-05 07:54:32 +0800

    feat: modified route.js - find message to be updated and do checking n return 404 response if no message. - check the ownership of the messages. - update the read field of the message.
     Changes to be committed:
    	modified:   app/api/messages/[id]/route.js

--- app/api/messages/[id]/route.js
+++ app/api/messages/[id]/route.js
@@ -1,3 +1,4 @@
+import Message from '@/components/Message';
 import connectDB from '@/config/database';
 import Messages from '@/models/Messages';
 import { getUserSession } from '@/utils/getUserSession';
@@ -11,15 +12,38 @@ export const PUT = async (request, { params }) => {
     // connect to db
     await connectDB();
 
-    const {id} = params
+    const { id } = params;
 
     // get user session from server
     const userSession = await getUserSession();
     // check if the session is set
     if (!userSession || !userSession.userId) {
-      return new Response('You need to login to use this function', { status: 401 });
+      return new Response('You need to login to use this function', {
+        status: 401,
+      });
     }
 
     const { userId } = userSession;
-  } catch (error) {}
+
+    const message = await Message.findById(id);
+
+    if (!message) {
+      return new Response('Message not found', { status: 404 });
+    }
+
+    // verify ownership
+    if (message.recipent.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    // update message to read / unread
+    message.read = !message.read;
+
+    message.save();
+
+    return new Response(JSON.stringify(message), {status: 200})
+  } catch (error) {
+    console.log(error);
+    return new Response('Error in updating message status', {status: 500})
+  }
 };
