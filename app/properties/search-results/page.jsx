e055be2372871fa5e6581f32120fafccc5d2bae0
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-05 15:54:58 +0800

    feat: add PropertySearchForm in search-results.

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -4,6 +4,7 @@ import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import { FaArrowAltCircleLeft } from 'react-icons/fa';
 import { PropertyCard } from '@/components/PropertyCard';
+import PropertySearchForm from '@/components/PropertySearchForm';
 import Spinner from '@/components/Spinner';
 
 const SearchResultsPage = () => {
@@ -58,22 +59,32 @@ const SearchResultsPage = () => {
   ) : (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
-        <Link href='/' className='flex items-center text-blue-500 hover:underline mb-3'>
-          <FaArrowAltCircleLeft className='mr-2 mb-1' /> Back to Home
+        <Link
+          href="/"
+          className="flex items-center text-blue-500 hover:underline mb-3"
+        >
+          <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back to Home
         </Link>
+        <section className="bg-blue-700 py-4 mb-4">
+          <div className="max-w-7x mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+            <PropertySearchForm />
+          </div>
+        </section>
         <h1 className="text-2xl mb-4">Search Result:</h1>
         {properties.length === 0 ? (
           showNoPropertyMessage ? (
-            <p className='text-2xl mb-4'>No properties found.</p>
+            <p className="text-2xl mb-4">No properties found.</p>
           ) : (
             <Spinner loading={true} />
           )
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
+          <>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          </>
         )}
       </div>
     </section>
