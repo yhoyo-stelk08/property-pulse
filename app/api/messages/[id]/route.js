aba3a64e2be72ee4601b3c041a58f9a180f3c1ab
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-05 09:01:11 +0800

    feat: add delete request to /api/messages/route.js

--- app/api/messages/[id]/route.js
+++ app/api/messages/[id]/route.js
@@ -41,9 +41,51 @@ export const PUT = async (request, { params }) => {
 
     message.save();
 
-    return new Response(JSON.stringify(message), {status: 200})
+    return new Response(JSON.stringify(message), { status: 200 });
   } catch (error) {
     console.log(error);
-    return new Response('Error in updating message status', {status: 500})
+    return new Response('Error in updating message status', { status: 500 });
+  }
+};
+
+// DELETE /api/messages/:id
+export const DELETE = async (request, { params }) => {
+  try {
+    // connect to db
+    await connectDB();
+
+    const { id } = params;
+
+    // get user session from server
+    const userSession = await getUserSession();
+    // check if the session is set
+    if (!userSession || !userSession.userId) {
+      return new Response('You need to login to use this function', {
+        status: 401,
+      });
+    }
+
+    const { userId } = userSession;
+
+    const message = await Messages.findById(id);
+
+    if (!message) {
+      return new Response('Message not found', { status: 404 });
+    }
+
+    // verify ownership
+    if (message.recipent.toString() !== userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    // if ownership verified
+    await message.deleteOne();
+
+    return new Response('Message Deleted', {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response('Error in deleting message', { status: 500 });
   }
 };
