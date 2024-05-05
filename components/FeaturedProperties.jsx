88a29188174fd64df089264f5b95bc01f4738a2e
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-05 21:48:00 +0800

    feat: add conditional rendering add conditional rendering to FeaturedProperties components that show featured properties only if properties length is greater than 0.
     Changes to be committed:
    	modified:   components/FeaturedProperties.jsx

--- components/FeaturedProperties.jsx
+++ components/FeaturedProperties.jsx
@@ -4,7 +4,7 @@ import FeaturedPropertyCard from '@/components/FeaturedPropertyCard';
 const FeaturedProperties = async () => {
   const properties = await fetchProperties({ showFeatured: true });
 
-  return (
+  return properties.length > 0 && (
     <section className="bg-blue-50 px-4 pt-6 pb-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
